f66cda1d8774a6eb2b5d3953652ff61645c1b234
zhangjiayi <jiayi.zhang@thoughtworks.com>
2024-03-13 17:44:03 +0800

    [tech]: add install key

--- packages/yi-ui/index.ts
+++ packages/yi-ui/index.ts
@@ -1,7 +1,18 @@
-import { App } from "vue";
 import components from "./component";
-
 // 全局引入
-export default (app: App) => {
-  components.forEach((c) => app.use(c));
+
+// 是否已安装标识
+const INSTALLED_KEY = Symbol("INSTALLED_KEY");
+// 组件库插件
+const ElementPlus = {
+  install(app: any) {
+    // 如果该组件库已经安装过了，则不进行安装
+    if (app[INSTALLED_KEY]) return;
+    // 将标识值设置为 true，表示已经安装了
+    app[INSTALLED_KEY] = true;
+    // 循环组件库中的每个组件进行安装
+    components.forEach((c) => app.use(c));
+  },
 };
+
+export default ElementPlus;
